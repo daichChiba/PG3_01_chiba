5ff367ee3f0eddb379da0795fd33436bd6ada1da
LC1D_17_Chiba_Daichi <146914435+daichChiba@users.noreply.github.com>
2024-10-09 11:24:42 +0900

    コメントアウトを追加した。

--- main.cpp
+++ main.cpp
@@ -6,9 +6,13 @@ int WageRecursive(int recurrentWage,int count, int recurrentMoney, int wageMoney
 	//if (recurrentMoney>100){
 	//	recurrentWage = recurrentWage * 2 - 50;
 	//}
+
+	//一般の合計金額(時給x今まで働いた時間)
 	wageMoney = wage * count;
+	//再帰の合計金額(今までの再帰の合計金額+今の再帰の時給)
 	recurrentMoney += recurrentWage;
 
+	//一般と再帰の比較
 	if (recurrentMoney>wageMoney){
 		printf("再帰のお金と時間  お金=%d,時給=%d,時間=%d\n", recurrentMoney, recurrentWage,count);
 		printf("一般のお金と時間  お金=%d,時給=%d,時間=%d\n", wageMoney, wage, count);
@@ -16,6 +20,7 @@ int WageRecursive(int recurrentWage,int count, int recurrentMoney, int wageMoney
 	} else{
 		printf("再帰のお金と時間  お金=%d,時給=%d,時間=%d\n", recurrentMoney, recurrentWage, count);
 		printf("一般のお金と時間  お金=%d,時給=%d,時間=%d\n", wageMoney, wage, count);
+		//もし、再帰の金額が一般の金額より小さかった場合、関数を呼び起こす
 		return WageRecursive(recurrentWage * 2 - 50, count + 1, recurrentMoney, wageMoney, wage);
 	}
 }
