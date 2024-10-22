b3b1b22a2808a57063bad6945b2de863efc6edd7
LC1D_17_Chiba_Daichi <146914435+daichChiba@users.noreply.github.com>
2024-10-22 12:10:21 +0900

    1_3完了

--- main.cpp
+++ main.cpp
@@ -1,7 +1,40 @@
 #include<stdio.h>
+#include<windows.h>
+#include<stdlib.h>
+
+
+typedef void(*PFunc)(int*,int,int);
+
+void SetTimeOut(PFunc P, int second, int dice, int playerNumber) {
+	Sleep(second *= 600);
+	P(&second,dice,playerNumber);
+}
+
+//コールバック関数
+void DispResult(int* s, int dice, int playerNumber) {
+	int answer;
+	answer = dice%2;
+	if (answer == playerNumber) {
+		printf("dice=%d\n", dice);
+		printf("正解\n");
+	} else {
+		printf("dice=%d\n", dice);
+		printf("不正解\n");
+	}
+
+}
 
 int main() {
-	printf("ああああ");
+	int playerNumber = 0;
+	printf("半なら0,丁なら1と入力せよ\n");
+	printf("半か丁か : ");
+	scanf_s("%d", &playerNumber);
+	printf("\n");
+	PFunc P;
+	int dice = rand() % 6 + 1;
+	P = DispResult;
+
+	SetTimeOut(P, 5,dice,playerNumber);
 
 	return 0;
 }
