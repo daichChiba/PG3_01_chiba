e74fa6dba0d25e1969d009a9e1bf1bfa02c7a523
LC1D_17_Chiba_Daichi <146914435+daichChiba@users.noreply.github.com>
2024-10-31 23:24:23 +0900

    02_01完了

--- main.cpp
+++ main.cpp
@@ -1,7 +1,16 @@
 #include<stdio.h>
-
+#include"Enemy.h"
 int main() {
-	printf("ああああ");
+	Enemy enemy_;
+	while (true){
+		enemy_.Update();
+		int s;
+		printf("0で次のフェーズ、１でループを抜ける\n");
+		scanf_s("%d",& s);
+		if (s==1){
+			break;
+		}
+	}
 
 	return 0;
 }
