ccc6485d3e477a16cb2e61555c2a44e5cead1bdd
LC1D_17_Chiba_Daichi <146914435+daichChiba@users.noreply.github.com>
2024-11-05 23:28:37 +0900

    02_04完了

---
+++ IShape.h
@@ -0,0 +1,11 @@
+#pragma once
+class IShape{
+public:
+	virtual void Size() = 0;
+	virtual void Draw() = 0;
+
+private:
+
+
+};
+
---
+++ Rectangle.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "IShape.h"
+class Rectangle : public IShape{
+public:
+	void Size() override;
+	void Draw() override;
+
+private:
+	float x = 20.0f;
+	float y = 10.0f;
+	float area;
+
+};
+
