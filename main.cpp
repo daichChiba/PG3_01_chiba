7fe65a03b3d5889632cc2de610dfe12e202178f1
LC1D_17_Chiba_Daichi <146914435+daichChiba@users.noreply.github.com>
2025-01-21 13:03:34 +0900

    やっている途中

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <string>
 #include <iostream>
+#include <vector>
 #include <list>
+using namespace std;
 
 using namespace std;
 
@@ -15,11 +18,32 @@ int main() {
 	};
 
 	printf("1970年山手線\n");
-	for (list<char> itr = rosen.begin();
-		itr != rosen.end();
-		itr++) {
-		cout << *itr << endl;
-		printf("%s", rosen[itr]);
+	for (auto itr = rosen.begin(); itr != rosen.end(); ++itr)
+	{
+		std::cout << *itr << "\n";
 	}
+	//rosen
+	printf("\n2019年山手線\n");
+	for (auto itr = rosen.begin(); itr != rosen.end(); ++itr) {
+		cout << *itr << endl;
+
+		// "日暮里"の後に"西日暮里"を追加する
+		if (strcmp(*itr, "日暮里") == 0) {
+			itr = rosen.insert(itr, "西日暮里");
+			++itr;
+		}
+	}
+	printf("\n2019年\n");
+
+	//要素の追加
+	for (std::list<char>::iterator itr = rosen.begin(); itr != rosen.end(); ++itr)
+	{
+		if (*itr == "Tabata")
+		{
+			itr = rosen.insert(itr, "Nishi-Nippori");
+			++itr;
+		}
+	}
+
 	return 0;
 }
