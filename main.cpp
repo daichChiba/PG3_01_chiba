4a9fe0a35ce0be9e2502e8b1817ebe488280ed62
LC1D_17_Chiba_Daichi <146914435+daichChiba@users.noreply.github.com>
2025-02-04 10:33:22 +0900

    完了

--- main.cpp
+++ main.cpp
@@ -1,7 +1,22 @@
-#include<stdio.h>
+#include <iostream>
+#include <thread>
+
+using namespace std;
+
+void PrintThread(uint32_t num) {
+
+	cout << "thread" << num << endl;
+}
 
 int main() {
-	printf("ああああ");
+
+	// マルチスレッドではある
+	thread t1(PrintThread, 1);
+	t1.join();
+	thread t2(PrintThread, 2);
+	t2.join();
+	thread t3(PrintThread, 3);
+	t3.join();
 
 	return 0;
 }
